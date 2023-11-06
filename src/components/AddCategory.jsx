0bbc1fd309783bd724f17df501d537257a33bb7a
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2023-11-06 07:23:47 -0700

    primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export const AddCategory = ( { onNewCategory } ) => {
+
+    const [ inputValue, setInputValue ] = useState('');
+
+    const onInputChange = ( { target} ) => {
+        setInputValue( target.value );
+    }
+
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+        if( inputValue.trim().length <=1) return;
+        // onAddCategories( categories => [ inputValue, ...categories ]);
+        onNewCategory( inputValue.trim() );
+    }
+
+  return (
+    <form onSubmit= { onSubmit }>
+      <input 
+      type="text"
+      placeholder="Buscar gifs"
+      value={ inputValue }
+      onChange={ onInputChange }
+      />
+    </form>
+    )
+}
